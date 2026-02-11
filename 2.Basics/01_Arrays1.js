a6c6ebae087d691438a8a41577afdb511ef00ec3
Vikrant more <morevikrant1154@gmail.com>
2026-02-11 09:21:01 +0530

    Arrays Info

---
+++ 2.Basics/01_Arrays1.js
@@ -0,0 +1,64 @@
+ 
+ // ********        ARRAYS  ***************// 
+ 
+
+ // Js Arrays are resizable and can contaim mix of the different datatypes 
+ // JavaScript array-copy operations create shallow copies.
+ //  (All standard built-in copy operations with any JavaScript objects
+ //  create shallow copies, rather than deep copies).
+
+/* SHALLOW COPIES - A shallow copy of an object is a copy whose properties share
+  the same references (point to the same underlying values) 
+ As a result, when you change either the source or the copy,
+  you may also cause the other object to change too. 
+ That behavior contrasts with the behavior of a  " DEEP COPY ", 
+ in which the source and copy are completely independent. */
+
+ const arr  = [2,3,4,5,true,"Vikrant"]
+ const arr1 = ["Vikrant","Viky"]
+  
+ const myArr  = [1,2,3,4,5,6]
+ const myArr2 = new Array(1,3,5,7,9)    // use the round bracket here
+ console.log(myArr[2]);   // 3          indexing of array starts from the 2
+
+ // METHODS OF ARRAY
+
+myArr.push(7)         // it will push the elment in the array at the last position 
+myArr.push(8)  // [ 1, 2, 3, 4, 5, 6, 7, 8]
+myArr.pop(8)   // simply last element in the array will be removed 
+myArr.pop(7)   // [ 1, 2, 3, 4, 5, 6]
+ 
+
+ myArr.unshift(9) // 9 will be at the starting of the array but this method is not optimized 
+ myArr.shift(9)   // it will remove the first element in the array 
+
+ console.log(myArr.includes(9));   // false this method gives the boolean result 
+ console.log(myArr.indexOf(9))   // -1 because the 9 is not present in the array 
+ 
+ const newArr = myArr.join()   //Adds all the elements of an array into a string, separated by the specified separator string.
+ console.log(newArr);  // 1,2,3,4,5,6    
+ console.log(typeof newArr);   // string
+
+ // Slice  
+  
+ console.log("A" , myArr);      // A [ 1, 2, 3, 4, 5, 6 ]
+ console.log(myArr.slice(1,3))   // [ 2, 3 ]          last index is not included 
+ console.log("B", myArr) // B [ 1, 2, 3, 4, 5, 6 ]   not change in org array after slice
+
+ // Splice
+
+ console.log(myArr.splice(1,3));  // [ 2, 3, 4 ]          last elment is included here
+ console.log("C" , myArr);  //    C [ 1, 5, 6 ]
+
+ // After the Splice operation the Original Array changed but not after Slice 
+  // thats the main difference    *Imp
+ 
+ 
+
+
+
+ 
+  
+ 
+
+ 
---
+++ 2.Basics/02_Arrays2.js
@@ -0,0 +1,60 @@
+const marvelHeroes = ["Spiderman","Thor","IronMan"]
+const dcHeroes = ["Batman","SuperMan","flash"]
+
+// Array in js can take any type of the data thats why it took Array as a data inside the Array
+// marvelHeroes.push(dcHeroes)
+console.log(marvelHeroes);  // [ 'Spiderman', 'Thor', 'IronMan', [ 'Batman', 'SuperMan', 'flash' ] ]
+
+//if we want to acess the element in the array then the 3rd element is the Array itself 
+// if we want to acess the element in that array then
+//console.log(marvelHeroes[3][1]);   // SuperMan
+// But we should not do the merge by this way 
+
+marvelHeroes.concat(dcHeroes)   // Combines two or more arrays. 
+// This method returns a new array without modifying any existing arrays.
+//console.log(marvelHeroes);       // its  not right because its not doing that properly 
+// so we should make a array to store the values 
+
+const allHeroes = marvelHeroes.concat(dcHeroes)
+console.log(allHeroes); // [ 'Spiderman', 'Thor', 'IronMan', 'Batman', 'SuperMan', 'flash' ]
+
+// Best way is Below  
+// this is the most common way to do this task
+
+const newHeroes = [...marvelHeroes,...dcHeroes]      // should be three dots 
+console.log(newHeroes); // [ 'Spiderman', 'Thor', 'IronMan', 'Batman', 'SuperMan', 'flash' ]
+
+const mixNums = [1,2,3,4,[[5,6,7],8,9]]
+// Now we want to make it all in the one array not like this mixed up
+
+const mixPrintN = mixNums.flat(3)   // have to give the depth in the bracket to open it 
+// we can also give the Infinity here in the bracket 
+console.log(mixPrintN); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+console.log(Array.isArray("Vikrant"))  // false for checking its array or not
+
+console.log(Array.from("Vikrant"))  // makes the new  array from the input given
+//  like this ['V', 'i', 'k','r', 'a', 'n','t']
+
+
+console.log(Array.from({name : "Vikrant"}));   // []     INTERESTING 
+// beacause  here we have to mention we have to make the Array from key or value 
+
+
+const o1 = 1622
+const o2  = 98989
+const o3 = 5656
+ // if we have to make the Array of this then
+
+ const newArr = Array.of(o1,o2,o3)       //[ 1622, 98989, 5656 ]
+ console.log(newArr);
+  
+console.log(Array.from("123"));   //  make the array of String chars
+// [ '1', '2', '3' ]
+
+console.log(Array.of(5));  // [5]
+
+console.log(Array(5));    // [ <5 empty items> ]       beacause Array contain nothing 
+
+console.log(Array.isArray([]));         // true 
