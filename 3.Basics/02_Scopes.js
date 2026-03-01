150d65ff7ccddac895bd567c6ec42e42670eed6e
Vikrant more <morevikrant1154@gmail.com>
2026-03-01 10:46:22 +0530

    scope applied

---
+++ 3.Basics/02_Scopes.js
@@ -0,0 +1,12 @@
+
+ if(true){
+let a = 32
+ const b = 232;
+ var c = 52;
+ console.log("Inner : ",a);  //Inner : 32  comma can also be used 
+ 
+ }
+//  console.log(a);
+//  console.log(b);        // they are block scoped 
+ console.log(c); // 52         here is the main problem with the var 
+
