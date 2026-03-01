d2e00a4deffba0504964df44e684e417128e81d6
Vikrant more <morevikrant1154@gmail.com>
2026-03-01 10:12:29 +0530

    function practiced

--- 3.Basics/01_Functions.js
+++ 3.Basics/01_Functions.js
@@ -1,28 +1,28 @@
 // # A function is a reusable block of code that does a specific task.
 //  Think of it like a recipe — you write it once, use it many times.
 
-function viky(){
-console.log("Vikrant More");
-console.log("From Amravati,Maharashtra");
+function viky(){                                
+console.log("Vikrant More");                        //  Vikrant More
+console.log("From Amravati,Maharashtra");          // From Amravati,Maharashtra
 }
-                                    //  Vikrant More
-                                    // From Amravati,Maharashtra
 viky()     
 // here above the viky is reference of function and            🔹 IMP
 //  if we want to execute then () this is execute 
 
 function addTwoNums(num1,num2){    // when we create the defintion of the funcn the values we pass are Parameters   
-    console.log(num1 + num2);
+    console.log(num1 + num2);       // parameters are actually waiting for the values 
 }
 
 addTwoNums(23323,1321)  //24644  if we dont pass the argument it will return the Nan 
 // when we are calling the functions then the value we pass are the arguments 
+// arguments are the real values given to the parameters 
 
 // if we write like this 
 
 function add(a,b){
     console.log(a +b);      // because here it is console log it only prints doesnt return
 }                           // return is different concept we  will see soon
+add(2,3)  //5
 const ans = add(5,3)
 console.log("answer is : " , ans); // answer is :  undefined
 
@@ -109,8 +109,8 @@ function handleUser(anyObject){
 
 // we can also directly pass the object there like without creating it seperately
 
-handleUser({
-    name : "Vikrant",
+handleUser({            // if we are passing the object directly inside the method 
+    name : "Vikrant", // no need to write the function handleuser , direct write handleuser
     age  : 22,
     native : "Pune"       // Username is Vikrant , native is Pune and his age is 22 
 })
@@ -130,3 +130,34 @@ console.log(getSecondVal(newArray))   // 35
 console.log(getSecondVal([233,200,300])); // 200
 
 // we will see the scope soon 
+
+
+const More ={
+    tech : "java",
+    year : 4,
+    place  : "Amaravati"
+}
+
+// function handleViky(object){
+//    return `${object.tech} tech ,${object.year},year studying and place is ${object.place}  `
+// }
+
+//console.log(handleViky(More))     // java tech ,4,year studying and place is Amaravati  
+
+ function CalcCartVal(num2,...num1){  // rest operator 
+return num2,num1;   // IMP Point
+ }      // Comma operator in evaluates both but return only the last value
+
+ console.log(CalcCartVal(2432,232,43,423,67,565)); 
+ 
+
+ const obj = {
+    name : "Vikrant",
+    age : 23,
+    place : "Amaravti"
+ }
+
+ const {name,...details} = obj   // this is how we can destructure it using the rest operator 
+ console.log(name);    //Vikrant
+ console.log(details);  // { age: 23, place: 'Amaravti' }
+ 
